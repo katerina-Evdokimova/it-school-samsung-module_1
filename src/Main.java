93aeae9b95ba1e60b9d77c235a61602a1c765abe
katerina-Evdokimova <eea2002_2107leto@mail.ru>
2023-09-08 23:43:46 +0300

    Topic 3. Fixed names, coordinates

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String person = "\uD83E\uDDD9\u200D";
-        int person_live = 3;
+        int personLive = 3;
 
         String monster = "\uD83E\uDDDF\u200D";
         String castle = "\uD83C\uDFF0";
-        int size = 5;
-        int person_x = size;
-        int person_y = 1;
+        int size = 3;
+        int personX = 1;
+        int personY = size;
+
 
 
         int step = 0;
-        String left_block = "| ";
-        String right_block = "|";
+        String leftBlock = "| ";
+        String rightBlock = "|";
         String wall = "+ —— + —— + —— +";
         // \n, \t - как спец символ(упомянуть)
-//        String output_str = "+ —— + —— + —— +\n"
-//                + "|    |    | \uD83C\uDFE0 |\n"
-//                + "+ —— + —— + —— +\n"
-//                + "|    | " + monster + " |    |\n"
-//                + "+ —— + —— + —— +\n"
-//                + "| " + person + " |    |    |\n"
-//                + "+ —— + —— + —— +";
+        String output_str = "+ —— + —— + —— +\n"
+                + "|    |    | \uD83C\uDFE0 |\n"
+                + "+ —— + —— + —— +\n"
+                + "|    | " + monster + " |    |\n"
+                + "+ —— + —— + —— +\n"
+                + "| " + person + " |    |    |\n"
+                + "+ —— + —— + —— +";
         String[][] board = new String[size][size];
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
-                board[x][y] = "  ";
+                board[y][x] = "  ";
             }
         }
 
@@ -38,10 +39,12 @@ public class Main {
         for (int i = 0; i <= count_monster; i++) {
             board[r.nextInt(size - 1)][r.nextInt(size)] = monster;
         }
-        int castle_x = 0;
-        int castle_y = r.nextInt(size);
 
-        board[castle_x][castle_y] = castle;
+        int castleX = r.nextInt(size);
+        int castleY = 0;
+
+
+        board[castleY][castleX] = castle;
 
 
         System.out.println("Привет! Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
@@ -53,50 +56,50 @@ public class Main {
         switch (answer) {
             case "ДА" -> {
 
-                int max_step = 2;
+                int maxStep = 2;
 
                 while (true) {
-                    board[person_x - 1][person_y - 1] = person;
+                    board[personY - 1][personX - 1] = person;
 //                    System.out.println(output_str);
-//                    for (int x = 0; x < size; x++) {
+//                    for (int y = 0; y < size; y++) {
 //                        System.out.println(wall);
-//                        for (int y = 0; y < size; y++) {
-////                            if (person_x - 1 == x && person_y - 1 == y) System.out.print(left_block + person + " ");
-////                            else System.out.print(left_block + "   ");
-//                            System.out.print(left_block + board[x][y] + " ");
+//                        for (int x = 0; x < size; x++) {
+////                            if (personY - 1 == y && personX - 1 == x) System.out.print(leftBlock + person + " ");
+////                            else System.out.print(leftBlock + "   ");
+//                            System.out.print(leftBlock + board[y][x] + " ");
 //                        }
-//                        System.out.println(right_block);
+//                        System.out.println(rightBlock);
 //                    }
 //                    System.out.println(wall);
 
                     for (String[] raw : board) {
                         System.out.println(wall);
                         for (String col : raw) {
-                            System.out.print(left_block + col + " ");
+                            System.out.print(leftBlock + col + " ");
                         }
-                        System.out.println(right_block);
+                        System.out.println(rightBlock);
                     }
                     System.out.println(wall);
 
 
-                    System.out.println("Live:\t" + person_live + "\n");
+                    System.out.println("Live:\t" + personLive + "\n");
 
                     System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;" +
-                            "\nКоординаты персонажа - (x: " + person_x + ", y: " + person_y + "))");
+                            "\nКоординаты персонажа - (x: " + personX + ", y: " + personY + "))");
                     int x = sc.nextInt();
                     int y = sc.nextInt();
                     System.out.println(x + ", " + y);
 
                     // проверка
-                    if (x != person_x && y != person_y) {
+                    if (x != personX && y != personY) {
                         System.out.println("Неккоректный ход");
-                    } else if (Math.abs(x - person_x) == 1 || Math.abs(y - person_y) == 1) {
-                        if (board[x - 1][y - 1].equals("  ")) {
-                            board[person_x - 1][person_y - 1] = "  ";
-                            person_x = x;
-                            person_y = y;
+                    } else if (Math.abs(x - personX) == 1 || Math.abs(y - personY) == 1) {
+                        if (board[y - 1][x - 1].equals("  ")) {
+                            board[personY - 1][personX - 1] = "  ";
+                            personX = x;
+                            personY = y;
                             step++;
-                            System.out.println("Ход корректный; Новые координаты: " + person_x + ", " + person_y +
+                            System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY +
                                     "\nХод номер: " + step);
                         } else {
                             System.out.println("Решите задачу.");
@@ -105,7 +108,7 @@ public class Main {
                         System.out.println("Координаты не изменены");
                     }
 
-                    if (person_live <= 0) {
+                    if (personLive <= 0) {
                         break;
                     }
                 }
